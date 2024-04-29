4703facd4cf36a9cf0850bd698b54dfce97072ea
koumikuni <masic.biscuit@gmail.com>
2024-04-30 00:34:10 +0900

    ラズパイ用のコードです

--- sensor.py
+++ sensor.py
@@ -2,12 +2,23 @@ import time
 import board
 import adafruit_lsm9ds1
 from pythonosc import udp_client
+import os
 
 # ポート番号のリスト
 ports = [11001, 11002, 11003, 11004, 11005]
 
+# ラズパイのルートディレクトリに「raspi_id.txt」ファイルを用意して、その中にそのラズパイが何番目のラズパイかを０〜４の数字半角で書いておく。
+try:
+    with open(os.path.expanduser('~/raspi_id.txt'), 'r') as file:
+        port_index = int(file.read().strip())  # Strip is used to remove any whitespace and newline characters
+        # Ensure the port_index is within the valid range
+        if port_index not in range(5):
+            raise ValueError("ラズパイの番号を0~4番目で「raspi_id.txt」に記入してや！")
+except (FileNotFoundError, ValueError) as e:
+    print(f"ポート番号の指定のエラーやで。とりあえず0番目ってことにしとくわ: {e}")
+    port_index = 0  # Default to 0 if there's an error
+
 # このラズベリーパイが使用するポート番号
-port_index = 0  # 0から始まるインデックスで指定
 port = ports[port_index]
 
 # OSCクライアントの設定
@@ -59,6 +70,7 @@ while True:
     client.send_message("/raspi/R/gyro", gyroR)
 
     # データの表示と送信ポートの確認
+    print(f'送信先ipアドレス:{ip}')
     print(f'ポート {port}: センサーL - 加速度: X={accelL[0]:.2f}, Y={accelL[1]:.2f}, Z={accelL[2]:.2f}, 磁気: X={magL[0]:.2f}, Y={magL[1]:.2f}, Z={magL[2]:.2f}, ジャイロ: X={gyroL[0]:.2f}, Y={gyroL[1]:.2f}, Z={gyroL[2]:.2f}')
     print(f'ポート {port}: センサーR - 加速度: X={accelR[0]:.2f}, Y={accelR[1]:.2f}, Z={accelR[2]:.2f}, 磁気: X={magR[0]:.2f}, Y={magR[1]:.2f}, Z={magR[2]:.2f}, ジャイロ: X={gyroR[0]:.2f}, Y={gyroR[1]:.2f}, Z={gyroR[2]:.2f}')
 
